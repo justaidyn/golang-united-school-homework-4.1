530c589fc881fff5907b222458f29ca61dbcd123
Aidyn Omarov <aidyn_o_b@mail.ru>
2022-05-16 17:46:14 +0600

    Wrote the REverseString function

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	output = string(runes)
 	return output
 }
